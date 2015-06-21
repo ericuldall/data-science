01a7ea62bfa0d253aa84cbefb007965443caaec7
root <root@eclectichectic.com>
2015-06-21 02:28:03 +0100

    updated node app

--- knn/web/app.js
+++ knn/web/app.js
@@ -4,11 +4,12 @@ var router = new emvc.Router;
     router.addRoute('Home', 'GET', '/:age/:height/:weight', function(req, res){
         var sys = require('sys')
         var exec = require('child_process').exec;
-        function puts(error, stdout, stderr) { sys.puts(stdout) }
-        exec("python /var/python/ga/data-science/knn.py "+req.params.age+" "+req.params.height+" "+req.params.weight, function(err, stdout, stderr){
+	var cmd = "python /var/python/data-science/knn.py "+(req.query.arg || '-r')+" "+req.params.age+" "+req.params.height+" "+req.params.weight;
+	console.log(cmd);
+        exec(cmd, function(err, stdout, stderr){
             console.log(err, stdout, stderr);
             // python /var/python/ga/data-science/knn.py 27 182 104
             res.send(stdout);
         });
     });
-    emvc.Listen(router);
+    emvc.Listen(router, 9999);
