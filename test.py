a07ad173d86dd6fb015c287dc641e2aaba44dd48
Eric Uldall <ericuldall@gmail.com>
2015-07-02 09:28:02 -0700

    added notebooks

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import importlib.machinery
-loader = importlib.machinery.SourceFileLoader("data", "lib/data.py")
-data = loader.load_module()
-list_math = data.ListMath([1, 2, 3, 4, 5])
-
-
-print(list_math.getRange());
